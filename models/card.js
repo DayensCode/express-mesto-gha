035bd6623bb29c617ab0402dca186a98e73b426b
Diana Nazarova <alllleksandrova@mail.ru>
2023-05-05 23:00:04 +0400

    fix:card model

--- models/card.js
+++ models/card.js
@@ -16,11 +16,11 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  likes: {
-    type: [mongoose.Schema.Types.ObjectId],
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     default: [],
-  },
+  }],
   createdAd: {
     type: Date,
     default: Date.now,
