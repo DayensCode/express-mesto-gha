ac9018989b1a9231734b3725d3500808d9d11b67
Diana Nazarova <alllleksandrova@mail.ru>
2023-05-06 00:35:34 +0400

    fix: 404 message

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const cardsRouter = require('./cards');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
-router.use('/*', (res) => {
-  res.status(404).send({ message: '404: Not Found' });
+router.use('/*', (req, res) => {
+  res.status(404).send();
 });
 
 module.exports = router;
